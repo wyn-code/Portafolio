a5ba14ccb7efc1a716b27e6d9b4d3e783bda2736
bmdev <bmdev@bm-debian.debian-bm>
2026-02-12 10:04:58 +0000

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'static', 
     vite: {
         plugins: [tailwindcss()],
-    }
+    },
+    
 });
